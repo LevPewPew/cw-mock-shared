d661c64c8ed6ea984f899c1440b2dd91dea8356e
Levente Koppany <levente.koppany@anz.com>
2022-07-07 09:13:45 +1000

    removed expired comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ export { Parrot } from "./components/parrot";
 
 export { default as store } from "./stores";
 
-// NEXT remove the CI steps that do the copying
-
 // NEXT in catwalk env, change names from silly things to ANZ-y things
